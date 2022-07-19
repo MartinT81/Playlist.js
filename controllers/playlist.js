bfc615b60ac8de51d8c5c4158a71ef53be31c018
Glitch (snapdragon-grand-woodpecker) <none>
2022-07-19 19:06:53 +0000

    📺⛩ Checkpoint ./controllers/playlist.js:46292486/1996 ./models/playlist-store.js:46292486/560 ./controllers/dashboard.js:46292486/924 ./views/playlist.hbs:46292486/293 ./views/partials/listsongs.hbs:46292486/153 ./routes.js:46292486/69

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,15 +1,26 @@
 'use strict';
 
 const logger = require('../utils/logger');
-const playlistCollection = require('../models/playlist-store.js');
+const playlistStore = require('../models/playlist-store');
 
 const playlist = {
   index(request, response) {
+    const playlistId = request.params.id;
+    logger.debug('Playlist id = ', playlistId);
     const viewData = {
       title: 'Playlist',
+      playlist: playlistStore.getPlaylist(playlistId),
     };
     response.render('playlist', viewData);
   },
 };
 
 module.exports = playlist;
+
+  deleteSong(request, response) {
+    const playlistId = request.params.id;
+    const songId = request.params.songid;
+    logger.debug(`Deleting Song ${songId} from Playlist ${playlistId}`);
+    playlistStore.removeSong(playlistId, songId);
+    response.redirect('/playlist/' + playlistId);
+  },
