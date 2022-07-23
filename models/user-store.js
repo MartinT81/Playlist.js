83d9322e5ee939ae6336f7b0e0f54c21c7aa2ea7
Glitch (snapdragon-grand-woodpecker) <none>
2022-07-23 18:25:18 +0000

    🌂🐟 Checkpoint ./models/user-store.js:46292486/644 ./models/user-store.json:46292486/389

---
+++ models/user-store.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const _=require('lodash');
+
+const jsonStore = require ('./json-store');
+
+const userStore = {
+  store: new JsonStore ('./models/user-store.json', {users: []}),
+  collection: 'users',
+  
+  getAllUsers() {
+    return this.store.findAll(this.collection);
+    
+  },
+  
+  addUser(user){
+    this.store.add(this.collection, user);
+    this.store.save();
+    
+  },
+  
+  getUserById(id){
+    return this.store.findBy(this.collection, {id: id});
+    
+  },
+  
+  getUserByEmail(email){
+    return this.store.fondBy(this.collection, {email: email});
+    
+  },
+  
+};
+
+module.exports = userStore;
