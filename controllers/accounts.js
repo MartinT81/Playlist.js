3ef19bb0a9f09d203ba4d87eb0b6086491531a7a
Glitch (snapdragon-grand-woodpecker) <none>
2022-07-25 18:29:47 +0000

    🐉🐩 Checkpoint ./controllers/accounts.js:46292486/1415 ./routes.js:46292486/1647 ./views/partials/menu.hbs:46292486/696

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -22,10 +22,44 @@ const accounts = {
   },
   
   logout (request, response){
-    response.cookie('playlist, '');
+    response.cookie('playlist', '');
+    response.redirect ('/');
     
-  }
+  },
   
-  ,
+  signup (request, response){
+    
+    const viewData = {
+      titel: 'login to the service',
+    };
+    
+    response.render('signup',  viewData);
+  },
   
-};
+  register(request, response) {
+    const user = request.body;
+    user.id = uuid.v1();
+    userstore.addUser(user);
+    logger.info(`registering ${user.email}`);
+    response.redirect('/');
+  },
+  
+    authenticate(request, response) {
+    const user = userstore.getUserByEmail(request.body.email);
+    if (user) {
+      response.cookie('playlist', user.email);
+      logger.info(`logging in ${user.email}`);
+      response.redirect('/dashboard');
+    } else {
+      response.redirect('/login');
+    }
+  },
+  
+  getCurrentUser(request){
+    const userEmail = request.cookies.playlist;
+    return userstore.getUserByEmail(userEmail);
+    
+  },
+  };
+
+module.exports = accounts;
